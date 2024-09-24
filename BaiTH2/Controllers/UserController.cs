3d3b34a1a86e27c0f7e67926f6b71b0e6bfc5092
PhanThiBachLy <ptbly99999@gmail.com>
2024-09-24 14:24:14 +0700

    Add project files.

---
+++ BaiTH2/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTH2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        public ActionResult Welcome2()
+        {
+            return View();
+        }
+    }
+}
---
+++ BaiTH2/Scripts/myJS/UserJS.js
@@ -0,0 +1,6 @@
+﻿function Homnay() {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    alert("Hôm nay là ngày " + date.getDate() + "tháng" + date.getMonth()
+        + "năm" + date.getFullYear());
+}
